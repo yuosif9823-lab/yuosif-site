bf06d9b6f87eb91c31d363181995c0cdacd36617
yuosif9823-lab <yuosif98237421@gmail.com>
2026-03-16 00:33:21 +0400

    Update script.js

--- script.js
+++ script.js
@@ -9,14 +9,25 @@ document.getElementById("ipResult").innerHTML=
 
 function checkConnection(){
 
-document.getElementById("connectionResult").innerHTML=
-"الاتصال بالشبكة يعمل بشكل طبيعي";
+let status=["الاتصال آمن","لا يوجد اختراق","الشبكة مستقرة"];
+
+let result=status[Math.floor(Math.random()*status.length)];
+
+document.getElementById("connectionResult").innerHTML=result;
 
 }
 
 function securityCheck(){
 
-document.getElementById("securityResult").innerHTML=
-"لم يتم اكتشاف تهديدات";
+let security=[
+"النظام آمن ✅",
+"لم يتم اكتشاف اختراق",
+"تم اكتشاف اتصال مشبوه ⚠️",
+"يوجد نشاط غير طبيعي"
+];
+
+let result=security[Math.floor(Math.random()*security.length)];
+
+document.getElementById("securityResult").innerHTML=result;
 
 }
